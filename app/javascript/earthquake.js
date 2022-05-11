2f0995e43eaffa17982deafc0d12cc111e696815
Cengiz Üçgül <cengaver0708@gmail.com>
2022-05-11 23:00:10 +0300

    learning and testing globe.gl javascript features

--- app/javascript/earthquake.js
+++ app/javascript/earthquake.js
@@ -1,3 +1,5 @@
+const ringAltitude = 0.0015
+
 const world = Globe()
   .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
   .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
@@ -10,7 +12,7 @@ globeMaterial.bumpScale = 12;
 new THREE.TextureLoader().load('//unpkg.com/three-globe/example/img/earth-water.png', texture => {
   globeMaterial.specularMap = texture;
   globeMaterial.specular = new THREE.Color('Gray');
-  globeMaterial.shininess = 15;
+  globeMaterial.shininess = 11;
 });
 
 setTimeout(() => { // wait for scene to be populated (asynchronously)
@@ -19,41 +21,44 @@ setTimeout(() => { // wait for scene to be populated (asynchronously)
 });
 
 
-// Gen random data
-/*
-const N = 10;
-const gData = [...Array(N).keys()].map(() => ({
-  lat: (Math.random() - 0.5) * 180,
-  lng: (Math.random() - 0.5) * 360,
-  maxR: Math.random() * 20 + 3,
-  propagationSpeed: (Math.random() - 0.5) * 20 + 1,
-  repeatPeriod: Math.random() * 2000 + 200
-}));
-*/
-const gData = [
+let gData = [
   {
     lat: 36.88429922,
     lng: 30.70221813,
-    maxR: 3,
-    propagationSpeed: -3,
-    repeatPeriod: 500,
-  },
-  {
-    lat: 40.88429922,
-    lng: 30.70221813,
-    maxR: 3,
-    propagationSpeed: -5,
-    repeatPeriod: 400
+    maxR: 1.5,
+    propagationSpeed: -0.9,
+    repeatPeriod: 350,
   }
 ]
 
 // const colorInterpolator = t => `rgba(255,100,50,${Math.sqrt(1-t)})`;
-const colorInterpolator = t => `rgba(255,255,0,1)`;
+const ringColor = t => `rgba(255,255,0,1)`;
 
 world.ringsData(gData)
-  .ringColor(() => colorInterpolator)
+  .ringColor(() => ringColor)
   .ringMaxRadius('maxR')
   .ringPropagationSpeed('propagationSpeed')
   .ringRepeatPeriod('repeatPeriod')
   .ringResolution(48)
-  .ringAltitude(0.002)
+  .ringAltitude(ringAltitude)
+
+
+
+world
+  .labelsData(gData)
+  .labelLat(d => d.lat)
+  .labelLng(d => d.lng)
+  .labelLabel(d => `<b>${d.lat}</b> earthquakes in the past month:<ul><li>Hello</li><li>HMeeeello</li></ul>
+                    <br><a href="google.com" target="_blank">Google.com</a>`)
+  .labelText(d => '')
+  .labelSize(0.5)
+  .labelResolution(3)
+  .labelDotRadius(d => d.maxR / 2)
+  .labelColor(() => 'rgba(255, 50, 50, 0.75)')
+  .labelResolution(64)
+  .labelAltitude(ringAltitude + 0.001) // prevent ring waves from cut. the label anim.
+  .onLabelRightClick( (label, event, options) => {
+    console.log(label)
+  })
+
+world.pointOfView({ lat: 36.88429922, lng: 30.70221813, altitude: 0.7 }, 2000)
